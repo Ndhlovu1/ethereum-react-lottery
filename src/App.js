115d2eb09cc86d7c1a3f7104d44dc706120f5b1d
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2024-04-23 17:32:56 +0200

    Update(feat) : Added contract code

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import web3 from "./required/web3";
 import "../src/ethe/lottery"
 import "./index.css"
 import lottery from "../src/ethe/lottery";
+
+const ethlogo = "https://cdn.decrypt.co/wp-content/uploads/2019/03/ethereum.png"
 class App extends Component {
 
   // constructor(props){
   //   super(props)
   //   this.state = {manager : ''}
   // }
-
+  
   //The above is a code refactoring of creating state
   state = {
     manager : '',
     players: [],
-    balance : ''
+    balance : '',
+    value : '',
+    message: ''
   }
 
   //Verify the component was loaded in the lifecyle
@@ -30,36 +34,95 @@ class App extends Component {
 
     }
 
+  onSubmit = async (event) => {
+
+    //Value of this is set by default in this arrow function
+    event.preventDefault()
+
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message:'Waiting on transaction success...' })
+
+    await lottery.methods.enters().send({
+      from: accounts[0],
+      value : web3.utils.toWei(this.state.value,"ether")
+    })
+
+    this.setState({ message:"Successfully entered lottery!" })
+
+  }
+
+  onClick = async () => {
+    const accounts = await web3.eth.getAccounts()
+
+    this.setState({message: "Waiting on transaction success..."})
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    })
+
+    this.setState({message: "A winner has been picked!"})
+
+  }
+
+
   render() {
 
     // web3.eth.getAccounts()
     //   .then(console.log)
 
     return (
-      <div className="container">
-        <center><h1>Lottery Contract</h1>
+      <>
+        <div className="container">
 
-          <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">MANAGED BY</h5>
-                <p className="card-text">{this.state.manager}</p>
-                {/* <button type="button" className="btn btn-primary" data-mdb-ripple-init>Button</button> */}
-              </div>
+          <div class="px-4 py-5 my-5 text-center">
+            <img src={ethlogo} height={100} width={100}/>
+            <h1 class="display-5 fw-bold text-body-emphasis">LOTTERY CONTRACT</h1>
+            <div class="col-lg-6 mx-auto">
+              <p class="lead mb-4">
+                MANAGED BY : {this.state.manager}
+              </p>
+            </div>
           </div>
 
-        </center>
 
-        <div className="col-md-4">
-          
-              {/* <h5 className="card-title">MANAGED BY</h5> */}<br/>
-              <p className="card-text">There are a total of {this.state.players.length}</p>
-              <p className="card-text">Victory Price : {web3.utils.fromWei(this.state.balance, 'ether')} ether</p>
-              {/* <button type="button" className="btn btn-primary" data-mdb-ripple-init>Button</button> */}
-         
+            <div className="card shadow-lg p-3 mb-5 bg-white rounded">
+                <div className="card-body">
+
+                  <h6 className="card-text">Participants : [{this.state.players.length}] people
+                  <br/><br/> Price : [{web3.utils.fromWei(this.state.balance, 'ether')} ether]</h6>
+                  <hr/>
+                  <h4>Join Lottery!</h4>
+
+                  <form onSubmit={this.onSubmit}>
+                    <div className="formgroup">
+                      <label for="">Amount of Ether to enter</label>
+                      <input
+                        value={this.state.value}
+                        onChange={event => this.setState({value : event.target.value})}
+                        required
+                      />
+                    </div>
+                    {/* Send a transaction into the Blockchain */}
+                    <button type="submit" class="btn btn-primary">Enter</button>
+                  </form>
+                  <br/>
+
+                  <hr/>
+                  
+                  <h4>Select Winner</h4>
+                  <button className="btn btn-success" onClick={this.onClick}>PICK</button>
+               
+
+                  <div className="alert alert-primary" style={{marginTop:"20px"}} role="alert">
+                    {this.state.message}
+                  </div>
+                </div>
+            </div>
+
 
         </div>
-
-      </div>
+      </>
     );
   
   }
